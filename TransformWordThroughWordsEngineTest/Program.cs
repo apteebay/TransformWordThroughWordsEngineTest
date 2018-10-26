7e2becebe67dff5842ba333af0d63030c53cb648
Andrew@Teebs020 <andrewTeebay@gmail.com>
2018-10-26 17:22:08 +0100

    Add project files.

---
+++ TransformWordThroughWordsEngineTest/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformWordThroughWordsEngineTest
+{
+    class Program
+    {
+        private static IEnumerable<string> getWords()
+        {
+            var words = new List<string>();
+            var filepath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace("file:\\",""),"Data");
+            filepath = Path.Combine(filepath, "words-english1.txt");
+
+            using (var reader = new StreamReader(filepath))
+            {
+                string word;
+                while ((word = reader.ReadLine()) != null) words.Add(word);
+            }
+
+
+            return words;
+        }
+
+        static void Main(string[] args)
+        {
+            var transformations = FindPath(getWords(), "Spin", "Spot");
+        }
+
+        static HashSet<string> Dictionary = new HashSet<string>();
+        //dictionary used to find the parent in every node in the graph and to avoid traversing an already traversed node
+        static Dictionary<string, string> parents = new Dictionary<string, string>();
+
+        public static List<string> FindPath(IEnumerable<string> input, string start, string end)
+        {
+            // Use IEnumerable<List> for imput
+            char[] allcharacters =
+
+    {'\\','0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+            // A P Teebay in final use all Printable Char (ASCII 32 - 126)
+            // A P Teebay Use Dictonary.UnionWith(input)
+            Dictionary.UnionWith(input);
+            List<string> currentFrontier = new List<string>();
+            List<string> nextFrontier = new List<string>();
+            currentFrontier.Add(start);
+            while (currentFrontier.Count > 0)
+            {
+                foreach (string s in currentFrontier)
+                {
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        foreach (char c in allcharacters)
+                        {
+                            StringBuilder newWordBuilder = new StringBuilder(s);
+                            newWordBuilder[i] = c;
+                            string newWord = newWordBuilder.ToString();
+                            if (Dictionary.Contains(newWord))
+                            {
+                                //avoid traversing a previously traversed node
+                                if (!parents.Keys.Contains(newWord))
+                                {
+                                    parents.Add(newWord.ToString(), s);
+                                    nextFrontier.Add(newWord);
+                                }
+
+                            }
+                            if (newWord.ToString() == end)
+                            {
+                                return ExtractPath(start, end);
+
+                            }
+                        }
+                    }
+                }
+                currentFrontier.Clear();
+                currentFrontier.Concat(nextFrontier);
+                nextFrontier.Clear();
+            }
+            throw new ArgumentException("The given dictionary cannot be used to get a path from start to end");
+        }
+
+        private static List<string> ExtractPath(string start, string end)
+        {
+            List<string> path = new List<string>();
+            string current = end;
+            path.Add(end);
+            while (current != start)
+            {
+                current = parents[current];
+                path.Add(current);
+            }
+            path.Reverse();
+            return path;
+        }
+
+    }
+}
